eb0bd2d3b783c506f5b7e3b922a923c68b2e35cc
Edward Amor <edward.amor3@gmail.com>
2022-03-11 22:16:31 +0000

    fix: update deploy script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,5 +1,15 @@
-from brownie import CRVFunder, accounts
+from brownie import CRVFunder, Factory, accounts
 
 
 def main():
-    return CRVFunder.deploy({"from": accounts[0]})
+    # load an account to use for deployment
+    deployer = accounts.load("dev")
+    # need to put this somewhere everyone can verify is correct
+    fallback_receiver = "0xc420C9d507D0E038BD76383AaADCAd576ed0073c"  # Curve Grant Council Multisig
+
+    # deploy the factory
+    factory = Factory.deploy(fallback_receiver, {"from": deployer, "priority_fee": "2 gwei"})
+    implementation = CRVFunder.deploy({"from": deployer, "priority_fee": "2 gwei"})
+
+    # set the implementation
+    factory.set_implementation(implementation, {"from": deployer, "priority_fee": "2 gwei"})
