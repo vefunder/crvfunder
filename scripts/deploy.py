0943f9339bd0f7b1f76ecfebf1456ae4f1ba189d
Edward Amor <edward.amor3@gmail.com>
2022-03-12 08:18:39 +0000

    fix: deploy script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,15 +1,25 @@
-from brownie import CRVFunder, Factory, accounts
+from brownie import AdminProxy, FundraisingGaugeV1, GaugeFactoryV1, accounts
+
+# can verify against existing ownership proxies
+# https://curve.readthedocs.io/ref-addresses.html#ownership-proxies
+# https://gov.curve.fi/t/cip-79-replacing-emergency-dao/2004
+OWNERSHIP_ADMIN = "0x40907540d8a6C65c637785e8f8B742ae6b0b9968"
+EMERGENCY_ADMIN = "0x467947EE34aF926cF1DCac093870f613C96B1E0c"
 
 
 def main():
     # load an account to use for deployment
     deployer = accounts.load("dev")
-    # need to put this somewhere everyone can verify is correct
-    fallback_receiver = "0xc420C9d507D0E038BD76383AaADCAd576ed0073c"  # Curve Grant Council Multisig
+
+    # deploy the ownership proxy
+    admin_proxy = AdminProxy.deploy(
+        OWNERSHIP_ADMIN, EMERGENCY_ADMIN, {"from": deployer, "priority_fee": "2 gwei"}
+    )
+
+    # deploy the implementation
+    implementation = FundraisingGaugeV1.deploy(
+        admin_proxy, {"from": deployer, "priority_fee": "2 gwei"}
+    )
 
     # deploy the factory
-    factory = Factory.deploy(fallback_receiver, {"from": deployer, "priority_fee": "2 gwei"})
-    implementation = CRVFunder.deploy({"from": deployer, "priority_fee": "2 gwei"})
-
-    # set the implementation
-    factory.set_implementation(implementation, {"from": deployer, "priority_fee": "2 gwei"})
+    GaugeFactoryV1.deploy(implementation, {"from": deployer, "priority_fee": "2 gwei"})
