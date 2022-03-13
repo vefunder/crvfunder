29f46a9860c8452284e7312f6ffa6c9d2d3bca0b
Gerrit Hall <gerrit@rezscore.com>
2022-03-12 18:01:32 -0800

    tests: Add integrative testing

--- tests/unitary/test_factory.py
+++ tests/unitary/test_factory.py
@@ -4,3 +4,9 @@ def test_deploy_new_gauge(alice, factory):
     assert factory.get_gauge_count() == 1
     assert factory.get_gauge_by_idx(0) == tx.return_value
     assert tx.events["NewGauge"].values() == [tx.return_value, alice, 2**256 - 1]
+
+
+def test_can_deploy_multiple_gauges(alice, factory):
+    for i in range(10):
+        factory.deploy_gauge(alice, 2**256 - 1, {"from": alice})
+    assert factory.get_gauge_count() == 10
