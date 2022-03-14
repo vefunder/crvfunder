a6f3f4e361f82ac3d091562815f2099d9a2790c9
Edward Amor <edward.amor3@gmail.com>
2022-03-14 05:24:40 +0000

    fix: deploy script use existing gauge proxy

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,20 +1,12 @@
-from brownie import AdminProxy, FundraisingGaugeV1, GaugeFactoryV1, accounts
-
-# can verify against existing ownership proxies
-# https://curve.readthedocs.io/ref-addresses.html#ownership-proxies
-# https://gov.curve.fi/t/cip-79-replacing-emergency-dao/2004
-OWNERSHIP_ADMIN = "0x40907540d8a6C65c637785e8f8B742ae6b0b9968"
-EMERGENCY_ADMIN = "0x467947EE34aF926cF1DCac093870f613C96B1E0c"
+from brownie import FundraisingGaugeV1, GaugeFactoryV1, accounts
 
 
 def main():
     # load an account to use for deployment
     deployer = accounts.load("dev")
 
-    # deploy the ownership proxy
-    admin_proxy = AdminProxy.deploy(
-        OWNERSHIP_ADMIN, EMERGENCY_ADMIN, {"from": deployer, "priority_fee": "2 gwei"}
-    )
+    # https://curve.readthedocs.io/ref-addresses.html#ownership-proxies
+    admin_proxy = "0x519AFB566c05E00cfB9af73496D00217A630e4D5"
 
     # deploy the implementation
     implementation = FundraisingGaugeV1.deploy(
